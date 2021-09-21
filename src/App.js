403a9f8660aa103da32813be905cff2e01abf496
lauraAlabau <arlaura3@gmail.com>
2021-09-21 20:09:39 +0200

    Iteration 1| Render a Simple List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import foods from "./foods.json";
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  //console.log(foods)
+  //console.log(foods[0].name)
 
+  const [food, setFood] = useState(foods)
+  return <div className="App">
+    <h1>Food List</h1>
+
+    {food.map((element) => {
+      return(
+        <div>
+          <p>{element.name}</p>
+          <img src={element.image} width={100} alt="" />
+        </div>
+      )
+    })}
+    
+  </div>;
+}
 export default App;
